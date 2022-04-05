f0de5c548c734c689061cf5c154ba98d96175610
Owen Palmer <palmeroxlii@gmail.com>
2022-04-05 18:28:07 +0100

    Changing handling of infinitely reducing terms.

--- src/AlgorithmDisplay.js
+++ src/AlgorithmDisplay.js
@@ -2,50 +2,89 @@ class AlgorithmDisplay extends Popup {
 
   constructor(term, alg) {
     super();
-    this.slides = applyToTerm(term, alg);
-    this.current_slide = 0;
-    this.max_slide = this.slides.length-1;
 
-    this.header = new HeaderWithCanvas(header.ypos, "Slide 1 of "+this.slides.length, header.size, "Back", header.topright.text_size);
+    this.cur_term = term;
+    this.alg = alg;
+    this.slides = [];
+
+    this.cur_slide = 0;
+    this.max_slide = 0;
+    this.state = [];
+    this.terminated = false;
+
+    this.header = new HeaderWithCanvas(header.ypos, "Slide 1 of 1", header.size, "Back", header.topright.text_size);
     this.header.back.pos.set(-100, -100);
+    this.next = new Button(1000, 250+this.header.ypos/2, 100, 100, ">", 40);
+    this.prev = new Button(100, 250+this.header.ypos/2, 100, 100, "<", 40);
 
-    for (let slide of this.slides) {
+    this.applyPrecomp();
+  }
+
+  addSlides(new_slides) {
+    for (let slide of new_slides) {
       slide[1] += "\n";
       slide[0].updateBlock();
       slide[0].pos.set(600-slide[0].size.x/2, 300+(this.header.ypos-slide[0].size.y)/2);
       slide[0].updateBlock();
     }
+    this.slides.push(...new_slides);
+    this.max_slide = this.slides.length-1;
+    this.header.text = "Slide "+(this.cur_slide+1)+" of "+(this.max_slide+1);
+  }
 
-    this.next = new Button(1000, 250+this.header.ypos/2, 100, 100, ">", 40);
-    this.prev = new Button(100, 250+this.header.ypos/2, 100, 100, "<", 40);
+  applyPrecomp() {
+    // Intro.
+    let slides = [];
+    slides.push([this.cur_term.copyBlock(), this.alg[3]]);
+    // Expand any macros.
+    let result = expandMacros(this.cur_term.copyBlock(), slides);
+    this.cur_term = result[0];
+    this.terminated = result[1];
+    // Any algorithm-specific pre-computation.
+    if (!this.terminated) result = this.alg[1](this.cur_term.copyBlock(), slides, this.state);
+    this.cur_term = result[0];
+    this.terminated = result[1];
+    // Add the slides to the display.
+    this.addSlides(slides);
+  }
+
+  applyStep() {
+    let slides = [];
+    let result = this.alg[2](this.cur_term.copyBlock(), slides, this.state);
+    this.cur_term = result[0];
+    this.terminated = result[1];
+    this.addSlides(slides);
   }
 
   display() {
     // The current slide.
     background("Silver");
-    this.slides[this.current_slide][0].display();
+    this.slides[this.cur_slide][0].display();
     noStroke();
     fill("Black");
     textSize(30);
     textAlign(CENTER, BOTTOM);
-    text(this.slides[this.current_slide][1], 0, 0, 1200, 600);
+    text(this.slides[this.cur_slide][1], 0, 0, 1200, 600);
     textAlign(CENTER, CENTER);
     // UI
     this.header.display();
-    if (this.current_slide !== this.max_slide) this.next.display();
-    if (this.current_slide !== 0) this.prev.display();
+    if (this.cur_slide !== this.max_slide || !this.terminated) this.next.display();
+    if (this.cur_slide !== 0) this.prev.display();
   }
 
   execute() {
     this.header.topright.checkHover(true);
-    this.next.checkHover(this.current_slide !== this.max_slide);
-    this.prev.checkHover(this.current_slide !== 0);
+    this.next.checkHover(this.cur_slide !== this.max_slide || !this.terminated);
+    this.prev.checkHover(this.cur_slide !== 0);
   }
 
   mousePressed() {
-    if (this.next.highlighted && this.current_slide !== this.max_slide) ++this.current_slide;
-    if (this.prev.highlighted && this.current_slide !== 0) --this.current_slide;
-    this.header.text = "Slide "+(this.current_slide+1)+" of "+(this.max_slide+1);
+    if (this.next.highlighted) {
+      if (this.cur_slide === this.max_slide) this.applyStep();
+      ++this.cur_slide;
+    }
+    if (this.prev.highlighted) --this.cur_slide;
+    this.header.text = "Slide "+(this.cur_slide+1)+" of "+(this.max_slide+1);
     if (this.header.topright.highlighted) popup = null;
   }
 
--- src/AlgorithmFunctions.js
+++ src/AlgorithmFunctions.js
@@ -1,52 +1,21 @@
 let algorithm_spec = [
-  [" Beta reduction \n Normal-order strategy ", noPrecomp, betaNorm,
+  [" Beta reduction \n Normal-order strategy ", betaPrecomp, betaNorm,
   "Beta-reducing the term using the normal-order (leftmost-outermost) strategy."],
-  [" Beta reduction \n Applicative-order strategy ", noPrecomp, betaAppl,
+  [" Beta reduction \n Applicative-order strategy ", betaPrecomp, betaAppl,
   "Beta-reducing the term using the applicative-order (leftmost-innermost) strategy."]];
 
 // Some general functions shared by the algorithms.
 
-function macroCheck(term) {
+function expandMacrosCount(term) {
   if (term instanceof MacroUse) {
     term.highlighted = true;
     return 1;
   }
   let macro_count = 0;
   for (let slot of term.slots) {
-    macro_count += macroCheck(slot);
+    macro_count += expandMacrosCount(slot);
   }
   return macro_count;
-}
-
-function macroExpand(term, slides) {
-  let macro_count = macroCheck(term instanceof MacroDef?term.slots[1]:term);
-  if (macro_count !== 0) {
-    slides.push([term, "Start by expanding any macros."]);
-    let expanded = term.expandMacros();
-    slides.push([expanded[0],
-      expanded[1]?"Any macros are now expanded.":expanded[2].replace("\n", " ")]);
-    return [expanded[0], expanded[1]];
-  }
-  return [term, true];
-}
-
-function redexCheckAppl(term) {
-  if (term instanceof TermApp) {
-    let tmp = redexCheckNorm(term.slots[0]);
-    if (tmp !== null) {tmp.push(0); return tmp;}
-    tmp = redexCheckNorm(term.slots[1]);
-    if (tmp !== null) {tmp.push(1); return tmp;}
-    if (term.slots[0] instanceof TermAbs && term.slots[0].slots[0] instanceof TermVar) {
-      term.highlighted = true;
-      term.slots[0].highlighted = true;
-      return [];
-    }
-    return null;
-  } else if (term instanceof TermAbs || term instanceof MacroDef) {
-    let tmp = redexCheckNorm(term.slots[1]);
-    if (tmp !== null) {tmp.push(1); return tmp;}
-    return null;
-  } else return null;
 }
 
 function redexCheckNorm(term) {
@@ -60,6 +29,25 @@ function redexCheckNorm(term) {
     if (tmp !== null) {tmp.push(0); return tmp;}
     tmp = redexCheckNorm(term.slots[1]);
     if (tmp !== null) {tmp.push(1); return tmp;}
+    return null;
+  } else if (term instanceof TermAbs || term instanceof MacroDef) {
+    let tmp = redexCheckNorm(term.slots[1]);
+    if (tmp !== null) {tmp.push(1); return tmp;}
+    return null;
+  } else return null;
+}
+
+function redexCheckAppl(term) {
+  if (term instanceof TermApp) {
+    let tmp = redexCheckNorm(term.slots[0]);
+    if (tmp !== null) {tmp.push(0); return tmp;}
+    tmp = redexCheckNorm(term.slots[1]);
+    if (tmp !== null) {tmp.push(1); return tmp;}
+    if (term.slots[0] instanceof TermAbs && term.slots[0].slots[0] instanceof TermVar) {
+      term.highlighted = true;
+      term.slots[0].highlighted = true;
+      return [];
+    }
     return null;
   } else if (term instanceof TermAbs || term instanceof MacroDef) {
     let tmp = redexCheckNorm(term.slots[1]);
@@ -139,96 +127,101 @@ function renameVars(term_old, term_new, rename_from, rename_active, rename_to) {
   }
 }
 
-// The entry point for running the algorithms.
-function applyToTerm(term, alg) {
-  let slides = [];
-  slides.push([term.copyBlock(), alg[3]]);
-  let expanded = macroExpand(term, slides);
-  if (expanded[1]) alg[2](expanded[0], slides);
-  return slides;
-}
-
 // The meat of each individual algorithm.
 
-function betaReduce(term, slides, strategy) {
-  let rename_id = 0;
-  for (let i = 0; i < 10; ++i) {
-    // Identify the redex.
-    term = term.copyBlock();
-    let redex_path = strategy(term);
-    if (redex_path === null) {
-      slides.push([term, "The term does not contain any "+(i===0?"":"more ")+"redexes, so it is fully beta-reduced."]);
-      return slides;
-    }
-    slides.push([term, "This is the "+(i===0?"first":"next")+" redex to be reduced."]);
-    // Check if variable renaming is needed.
-    term = term.copyBlock();
-    let redex = term;
-    let path = redex_path.slice();
-    while (path.length > 0) {
-      redex = redex.slots[path.pop()];
-    }
-    let vars_arg = renameSearchFrees(redex.slots[1], []), vars_fun = renameSearchBinders(redex.slots[0]);
-    let rename_from = renameCheck(vars_arg, vars_fun);
-    if (rename_from.length > 0) {
-      // Highlight the old term's variables, and rename the new term's variables.
-      let term_old = term.copyBlock();
-      let redex_old = term_old;
-      path = redex_path.slice();
-      while (path.length > 0) {
-        redex_old = redex_old.slots[path.pop()];
-      }
-      let rename_to = [];
-      for (let i of rename_from) {
-        rename_to.push("$"+(rename_id++));
-      }
-      renameVars(redex_old.slots[0], redex.slots[0], rename_from, [], rename_to);
-      slides.push([term_old, "Some renaming is needed to avoid variable capture."]);
-      slides.push([term, "Any problematic variable binders have been renamed"]);
-      term = term.copyBlock();
-    }
-    // Now perform the reduction step.
-    redex = term;
+function expandMacros(term, slides, state) {
+  let macro_count = expandMacrosCount(term instanceof MacroDef?term.slots[1]:term);
+  if (macro_count !== 0) {
+    slides.push([term, "Start by expanding any macros."]);
+    let expanded = term.expandMacros();
+    slides.push([expanded[0],
+      expanded[1]?"Any macros are now expanded.":expanded[2].replace("\n", " ")]);
+    return [expanded[0].copyBlock(), !expanded[1]];
+  }
+  return [term, false];
+}
+
+function noPrecomp(term, slides, state) {
+  return [term, false];
+}
+
+function betaPrecomp(term, slides, state) {
+  state.rename_id = 0;
+  state.step_num = 0;
+  return [term, false];
+}
+
+function betaNorm(term, slides, state) {
+  return betaReduce(term, slides, state, redexCheckNorm);
+}
+function betaAppl(term, slides, state) {
+  return betaReduce(term, slides, state, redexCheckAppl);
+}
+
+function betaReduce(term, slides, state, strategy) {
+  // Identify the redex.
+  let redex_path = strategy(term);
+  if (redex_path === null) {
+    slides.push([term, "The term does not contain any "+(state.step_num===0?"":"more ")+"redexes, so it is fully beta-reduced."]);
+    return [term, true];
+  }
+  slides.push([term, "This is the "+(state.step_num===0?"first":"next")+" redex to be reduced."]);
+  state.step_num++;
+  // Check if variable renaming is needed.
+  term = term.copyBlock();
+  let redex = term;
+  let path = redex_path.slice();
+  while (path.length > 0) {
+    redex = redex.slots[path.pop()];
+  }
+  let vars_arg = renameSearchFrees(redex.slots[1], []), vars_fun = renameSearchBinders(redex.slots[0]);
+  let rename_from = renameCheck(vars_arg, vars_fun);
+  if (rename_from.length > 0) {
+    // Highlight the old term's variables, and rename the new term's variables.
+    let term_old = term.copyBlock();
+    let redex_old = term_old;
     path = redex_path.slice();
     while (path.length > 0) {
-      redex = redex.slots[path.pop()];
+      redex_old = redex_old.slots[path.pop()];
     }
-    redex.highlighted = true;
-    redex.slots[0].highlighted = true;
-    slides.push([term, (rename_from.length > 0)?"Now reduce the redex.":"No variable renaming is needed, so just reduce the redex."]);
+    let rename_to = [];
+    for (let i of rename_from) {
+      rename_to.push("$"+(state.rename_id++));
+    }
+    renameVars(redex_old.slots[0], redex.slots[0], rename_from, [], rename_to);
+    slides.push([term_old, "Some renaming may be needed to avoid variable capture."]);
+    slides.push([term, "Any potentially problematic variable binders have been renamed."]);
     term = term.copyBlock();
-    if (redex_path.length === 0) {
-      // The entire term is the redex.
-      let vari = term.slots[0].slots[0].text;
-      let func = term.slots[0].slots[1];
-      let argu = term.slots[1];
-      term = redexSubstitute(func, argu, vari);
-    } else {
-      // The redex is within the term.
-      let parent = term;
-      path = redex_path.slice();
-      while (path.length > 1) {
-        parent = parent.slots[path.pop()];
-      }
-      redex = parent.slots[path[0]];
-      let vari = redex.slots[0].slots[0].text;
-      let func = redex.slots[0].slots[1];
-      let argu = redex.slots[1];
-      parent.slots[path[0]] = redexSubstitute(func, argu, vari);
-    }
-    slides.push([term, "The redex has now been reduced."]);
   }
-  slides.push([term.copyBlock(),
-    "The term has been reduced 10 times without stopping. For the sake of safety, the algorithm will be halted here."]);
-  return slides;
-}
-
-function noPrecomp(term, slides) {
-}
-
-function betaNorm(term, slides) {
-  betaReduce(term, slides, redexCheckNorm);
-}
-function betaAppl(term, slides) {
-  betaReduce(term, slides, redexCheckAppl);
+  // Now perform the reduction step.
+  redex = term;
+  path = redex_path.slice();
+  while (path.length > 0) {
+    redex = redex.slots[path.pop()];
+  }
+  redex.highlighted = true;
+  redex.slots[0].highlighted = true;
+  slides.push([term, (rename_from.length > 0)?"Now reduce the redex.":"No variable renaming is needed, so just reduce the redex."]);
+  term = term.copyBlock();
+  if (redex_path.length === 0) {
+    // The entire term is the redex.
+    let vari = term.slots[0].slots[0].text;
+    let func = term.slots[0].slots[1];
+    let argu = term.slots[1];
+    term = redexSubstitute(func, argu, vari);
+  } else {
+    // The redex is within the term.
+    let parent = term;
+    path = redex_path.slice();
+    while (path.length > 1) {
+      parent = parent.slots[path.pop()];
+    }
+    redex = parent.slots[path[0]];
+    let vari = redex.slots[0].slots[0].text;
+    let func = redex.slots[0].slots[1];
+    let argu = redex.slots[1];
+    parent.slots[path[0]] = redexSubstitute(func, argu, vari);
+  }
+  slides.push([term, "The redex has now been reduced."]);
+  return [term.copyBlock(), false];
 }
